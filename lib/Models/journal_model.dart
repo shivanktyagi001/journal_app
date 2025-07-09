7231fddad1b5b974392319aed47879479a349c3a
shivanktyagi_01 <shivankty114@gmail.com>
2025-07-09 15:37:25 +0530

    Add JournalModel

---
+++ lib/Models/journal_model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+class JournalModel{
+  String title;
+  String des;
+  DateTime datetime;
+  String tag;
+  String mood;
+  JournalModel({
+     required this.title,
+    required this.des,
+    required this.datetime,
+    required this.mood,
+    required this.tag,
+});
+  Map<String,dynamic> tomap(){
+    return {
+      'title':title,
+      'des':des,
+      'datetime':datetime,
+      'tag':tag,
+      'mood':mood,
+    };
+  }
+  factory JournalModel.frommap(Map<String,dynamic>mp){
+    return JournalModel(title:mp['title'] ,
+        des: mp['des'], datetime: mp['datetime'], mood: mp['mood'], tag: mp['tag']);
+  }
+}
